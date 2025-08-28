7ad56b80ec3f84715a11da26353ecb79283d5666
Muhammad Talal Aslam <146806220+ChaudharyTalal@users.noreply.github.com>
2025-08-28 20:14:19 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-//core module
-const path=require('path')
-//extrenal module
+// core modules
+const path = require("path");
+//external modules
+const express = require("express");
+//local Router
+const userRouter = require("./routes/userRouter");
+const {hostRouter} = require("./routes/hostRouter");
+const rootDir=require("./utils/pathUtil")
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const app = express();
+app.set('view engine','ejs')
+app.set('views','views')
 
-//local router
-const homeRouter=require('./routes/home')
-const ContactRouter=require('./routes/contact')
-const rootDir=require('./utils/pathutils')
+// app.use((req, res, next) => {
+//     console.log(req.url, req.method);
+//     next();
+// });
+
+
 
 
 app.use(express.urlencoded());
-app.use(homeRouter)
-app.use(bodyParser.urlencoded())
-app.use(ContactRouter)
+app.use(userRouter);
+app.use("/host", hostRouter);
 
+// use public folder files
 app.use(express.static(path.join(rootDir,'public')))
 
-
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    res.status(404).render('404',{pageTitle:"404 Page not found"})
 });
 
-
-module.exports=app;
-
-
+const PORT = 1000;
+app.listen(PORT, () => {
+    console.log(`server running on address address http://localhost:${PORT}`);
+});
